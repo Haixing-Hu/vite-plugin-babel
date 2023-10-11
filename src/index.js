939c2ddd30425a16c25969733a41cb272305d733
Haixing Hu <starfish.hu@gmail.com>
2023-10-11 17:18:19 +0800

    fix: fix bugs

--- src/index.js
+++ src/index.js
@@ -10,9 +10,11 @@ import * as babel from '@babel/core';
 
 const DEFAULT_FILTER = /\.(jsx?|vue)$/;
 
-const babelPlugin = ({ config = {}, filter = DEFAULT_FILTER }) => ({
+const babelPlugin = (options = {}) => ({
   name: 'vite-plugin-babel',
   transform: (src, id) => {
+    const config = options.config || {};
+    const filter = options.filter || DEFAULT_FILTER;
     if (filter.test(id)) {
       config.filename = id;
       return babel.transform(src, config);
