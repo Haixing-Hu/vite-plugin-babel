391f9de78aa93c8f12ddceb3a3e5f32070835d28
Haixing Hu <starfish.hu@gmail.com>
2024-03-15 22:23:24 +0800

    feat: add some logging statements

--- src/index.js
+++ src/index.js
@@ -10,19 +10,26 @@ import * as babel from '@babel/core';
 
 const DEFAULT_FILTER = /\.(jsx?|vue)$/;
 
-const babelPlugin = (options = {}) => ({
-  name: 'vite-plugin-babel',
-  enforce: 'post',
-  transform: (src, id) => {
-    const config = options.config || {};
-    const filter = options.filter || DEFAULT_FILTER;
-    if (filter.test(id)) {
-      config.filename = id;
-      console.info(`Transforming file ${id} with config:`, config);
-      return babel.transform(src, config);
-    }
-    return null;
-  },
-});
+const babelPlugin = (options = {}) => {
+  console.info('[vite-plugin-babel] Loading vite-plugin-babel with options:', options);
+  return {
+    name: 'vite-plugin-babel',
+    enforce: 'post',
+    transform: (src, id) => {
+      console.info(`[vite-plugin-babel] Transforming file ${id}...`);
+      const config = options.config || {};
+      const filter = options.filter || DEFAULT_FILTER;
+      if (filter.test(id)) {
+        config.filename = id;
+        console.info(`[vite-plugin-babel] The file ${id} will be transformed with config:`, config);
+        return babel.transform(src, config);
+      }
+      return null;
+    },
+    buildEnd: () => {
+      console.info('[vite-plugin-babel] The build process is finished.');
+    },
+  };
+};
 
 export default babelPlugin;
